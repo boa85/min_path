962c177bf1531666fcb4b10bcf375d06c082b2fb
boa <boa@promobit.ru>
2017-08-09 02:55:13 +0600

    added class CommandHandler with basic functional

---
+++ command_handler/include/command_handler.hpp
@@ -0,0 +1,70 @@
+//
+// Created by boa on 09.08.17.
+//
+
+#ifndef MIN_PATH_COMMAND_HANDLER_HPP
+#define MIN_PATH_COMMAND_HANDLER_HPP
+
+#include <string>
+#include <functional>
+#include <memory>
+
+
+#include "icommand.hpp"
+
+namespace min_path {
+
+    namespace command_handler {
+
+        /**
+ * @class CommandHandler  - in classical description (GoF) Receiver, executes commands
+ */
+        class CommandHandler {
+        public:
+            /**
+             * @brief CommandHandler - constructor
+             */
+            CommandHandler() = default;
+
+            /**
+             * @brief ~CommandHandler - destructor
+             */
+            ~CommandHandler() = default;
+
+            /**
+             * @brief CommandHandler -  copy constructor
+             */
+            CommandHandler(const CommandHandler &) = default;
+
+            /**
+             * @brief  - move constructor
+             */
+            CommandHandler(CommandHandler &&) = default;
+
+            /**
+             * @brief operator= copy assignment operator
+             */
+            CommandHandler &operator=(const CommandHandler &) = default;
+
+            /**
+             * @brief operator= move assignment operator
+             */
+            CommandHandler &operator=(CommandHandler &&) = default;
+
+            /**
+             * @brief start - run command execution of the command
+             * @param command - command to be executed
+             */
+            void start(std::shared_ptr<ICommand> command);
+
+
+        }//namespace command_handler
+
+    }//namespace min_path
+
+    class command_handler {
+
+    };
+
+
+#endif //MIN_PATH_COMMAND_HANDLER_HPP
---
+++ command_handler/src/command_handler.cpp
@@ -0,0 +1,17 @@
+//
+// Created by boa on 09.08.17.
+//
+
+#include "../include/command_handler.hpp"
+
+namespace min_path {
+
+    namespace command_handler {
+
+        void CommandHandler::start(std::shared_ptr<ICommand> command) {
+            command->execute();
+        }//start
+
+    }//namespace command_handler
+
+}//namespace min_path
