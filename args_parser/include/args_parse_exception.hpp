d0d8f69f94e7e072b97a7a4a0b79fc9b2a661de9
boa <boa@promobit.ru>
2017-08-09 01:32:52 +0600

    added ArgsParseException class implementation

--- args_parser/include/args_parse_exception.hpp
+++ args_parser/include/args_parse_exception.hpp
@@ -4,10 +4,51 @@
 
 #ifndef MIN_PATH_ARGS_PARSE_EXCEPTION_HPP
 #define MIN_PATH_ARGS_PARSE_EXCEPTION_HPP
+
+#include <exception>
+#include <string>
+#include <stdexcept>
+
 namespace min_path {
     namespace argument_parser {
-        class ArgsParseException {
+        /**
+         * @class ArgsParseException - my exception
+         */
+        class ArgsParseException : public std::logic_error {
+        public:
+            /**
+             * @brief ArgsParseException  - constructor
+             * @param message - error message
+             */
+            explicit ArgsParseException(const std::string &message);
 
+            /**
+             * @brief ArgsParseException - constructor
+             * @param message
+             */
+            explicit ArgsParseException(const char *message);
+
+            /**
+             * @brief ArgsParseException - copy constructor
+             */
+            ArgsParseException(const ArgsParseException &) = default;
+
+            /**
+             * @param ArgsParseException - copy assignment operator
+             */
+            ArgsParseException &operator=(const ArgsParseException &) = default;
+
+            /**
+             * @brief ArgsParseException - destructor
+             */
+            ~ArgsParseException() override = default;
+
+            /**
+             * @brief what - override method
+             * @return Returns a C-style character string describing the general cause of
+             * the current error (the same string passed to the ctor).
+             */
+            const char *what() const throw() override;
         };//class ArgsParseException
     }// argument_parser
 }//min_path
--- args_parser/src/args_parse_exception.cpp
+++ args_parser/src/args_parse_exception.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "../include/args_parse_exception.hpp"
+
+namespace min_path {
+    namespace argument_parser {
+        ArgsParseException::ArgsParseException(const std::string &message)
+                : std::logic_error(message) {}
+
+        const char *ArgsParseException::what() const throw() {
+            return std::logic_error::what();
+        }
+
+        ArgsParseException::ArgsParseException(const char *message)
+                : std::logic_error(message) {}
+
+    }// argument_parser
+}//min_path
