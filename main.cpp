777eb6216df70cfa4ea133db6570027417f24343
boa <boa@promobit.ru>
2017-08-10 17:40:40 +0600

    added basic implementation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <memory>
+
+#include "args_parser/include/argument_parser.hpp"
+#include "command_handler/include/command_handler.hpp"
+#include "command_handler/include/invoker.hpp"
+#include "args_parser/include/args_parse_exception.hpp"
 
 /**
  * Есть файл каждая строка которого имеет формат:
@@ -8,7 +14,25 @@
  * а на выходе выдавать минимальное расстояние между вершинами
  * и распечатывать маршрут между данными вершинами.
  */
+using namespace min_path;
+using namespace argument_parser;
+using namespace command_handler;
 int main(int argc, char *argv[]) {
-    std::cout << "Hello, World!" << std::endl;
+    try {
+        auto parser = std::make_shared<ArgumentParser>();
+        auto invoker = std::make_shared<Invoker>();
+        auto commandHandler = std::make_shared<CommandHandler>();
+        parser->generateGraph.connect(boost::bind(&Invoker::generateGraph, invoker, _1, _2, _3));
+        parser->findShortestPath.connect(boost::bind(&Invoker::findShortestPath, invoker, _1, _2, _3));
+        invoker->start.connect(boost::bind(&CommandHandler::start, commandHandler, _1));
+        parser->startParsing(argc, argv);
+    }
+    catch (ArgsParseException &e) {
+        std::cerr << e.what() << std::endl;
+    }
+    catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+
     return 0;
 }
