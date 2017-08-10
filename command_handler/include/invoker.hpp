fa69e85c61bc69f01cdd175375862cac405a491a
boa <boa@promobit.ru>
2017-08-10 17:38:56 +0600

    added implementation of all methods

--- command_handler/include/invoker.hpp
+++ command_handler/include/invoker.hpp
@@ -5,10 +5,15 @@
 #ifndef MIN_PATH_INVOKER_HPP
 #define MIN_PATH_INVOKER_HPP
 
+#include <boost/signals2/signal.hpp>
+#include <memory>
+
+#include "icommand.hpp"
 
 namespace min_path {
 
     namespace command_handler {
+        namespace bs=boost::signals2;
         /**
          * @class Invoker - Based on the input data received from the client,
          * it generates commands and passes it on to execution
@@ -26,7 +31,7 @@ namespace min_path {
             virtual  ~Invoker() = default;
 
             /**
-             * @brief Invoker - copy contructor
+             * @brief Invoker - copy constructor
              */
             Invoker(const Invoker &) = default;
 
@@ -45,8 +50,49 @@ namespace min_path {
              */
             Invoker &operator=(Invoker &&) = default;
 
-        };
+            /**
+            * @brief start - boost signal for sending command to CommandHandler
+            */
+            bs::signal<void(std::shared_ptr<ICommand> &command)> start;
 
+            /**
+             * @brief generateGraph create GenerateGraphCommand  and send to CommandHandler
+             * @param filename - input filename
+             * @param vertexNumber - number of vertex
+             * @param probability - probability of appearance of an edge between vertices in percent
+             */
+            void generateGraph(const std::string &filename, unsigned int vertexNumber, unsigned int probability);
+
+            /**
+             * @brief findShortestPath create FindShortestCommand and send to CommandHandler
+             * @filename - input filename
+             * @inVertex - input vertex number
+             * @outVertex - output vertex number
+             */
+            void findShortestPath(const std::string &filename, unsigned int inVertex, unsigned int outVertex);
+
+        private:
+            /**
+             * @brief createGenerateGraphCommand - create new exemplar of GenerateGraphCommand
+             * @param filename - input filename
+             * @param vertexNumber - number of vertex
+             * @param probability - probability of appearance of an edge between vertices in percent
+             * @return new exemplar of GenerateGraphCommand class
+             */
+            std::shared_ptr<ICommand> createGenerateGraphCommand(const std::string &filename, unsigned int vertexNumber,
+                                                                 unsigned int probability);
+
+            /**
+             * @brief create createFindShortestCommand and send to CommandHandler
+             * @filename - input filename
+             * @inVertex - input vertex number
+             * @outVertex - output vertex number
+             * @return new exemplar of FindShortestCommand class
+             */
+            std::shared_ptr<ICommand> createFindShortestCommand(const std::string &filename, unsigned int inVertex,
+                                                                unsigned int outVertex);
+
+        };//class Invoker
 
     }//namespace command_handler
 
--- command_handler/src/invoker.cpp
+++ command_handler/src/invoker.cpp
@@ -3,10 +3,36 @@
 //
 
 #include "../include/invoker.hpp"
+#include "../include/generate_graph_command.hpp"
+#include "../include/shortest_path_command.hpp"
 
 namespace min_path {
 
     namespace command_handler {
+
+        void Invoker::generateGraph(const std::string &filename, const unsigned int vertexNumber,
+                                    const unsigned int probability) {
+            auto command = createGenerateGraphCommand(filename, vertexNumber, probability);
+            start(command);
+        }//generateGraph
+
+        void Invoker::findShortestPath(const std::string &filename, const unsigned int inVertex,
+                                       const unsigned int outVertex) {
+            auto command = createFindShortestCommand(filename, inVertex, outVertex);
+            start(command);
+        }//findShortestPath
+
+        std::shared_ptr<ICommand> Invoker::createGenerateGraphCommand(const std::string &filename,
+                                                                      const unsigned int vertexNumber,
+                                                                      const unsigned int probability) {
+            return std::make_shared<GenerateGraphCommand>(filename, vertexNumber, probability);
+        }//createGenerateGraphCommand
+
+        std::shared_ptr<ICommand> Invoker::createFindShortestCommand(const std::string &filename,
+                                                                     const unsigned int inVertex,
+                                                                     const unsigned int outVertex) {
+            return std::make_shared<FindShortestPathCommand>(filename, inVertex, outVertex);
+        }//createFindShortestCommand
 
     }//namespace command_handler
 
