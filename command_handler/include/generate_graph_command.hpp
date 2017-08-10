8a0f2e0e493146706499cd8bbe4892c3c2241c1f
boa <boa@promobit.ru>
2017-08-10 17:37:16 +0600

    added  implementation  Constructor and execute

--- command_handler/include/generate_graph_command.hpp
+++ command_handler/include/generate_graph_command.hpp
@@ -3,6 +3,8 @@
 #ifndef MIN_PATH_GENERATE_GRAPH_COMMAND
 #define MIN_PATH_GENERATE_GRAPH_COMMAND
 
+#include <string>
+
 #include "icommand.hpp"
 
 namespace min_path {
@@ -10,17 +12,24 @@ namespace min_path {
     namespace command_handler {
         class GenerateGraphCommand : public ICommand {
         public:
+            GenerateGraphCommand(const std::string &filename, unsigned int vertexNumber,
+                                 unsigned int probability);
             ~GenerateGraphCommand() override = default;
 
             GenerateGraphCommand(const GenerateGraphCommand &command) = default;
 
             GenerateGraphCommand(GenerateGraphCommand &&command) = default;
 
-            GenerateGraphCommand &operator=(const GenerateGraphCommand &command) = default;
+            GenerateGraphCommand &operator=(const GenerateGraphCommand &command) = delete;
 
-            GenerateGraphCommand &operator=(GenerateGraphCommand &&command)  = default;
+            GenerateGraphCommand &operator=(GenerateGraphCommand &&command)  = delete;
 
             void execute() override;
+
+        private:
+            const std::string filename_;
+            const unsigned int vertexNumber_;
+            const unsigned int probability_;
         };
 
     }//namespace command_handler
--- command_handler/src/generate_graph_command.cpp
+++ command_handler/src/generate_graph_command.cpp
@@ -6,14 +6,16 @@ namespace min_path {
 
     namespace command_handler {
 
-        GenerateGraphCommand::~GenerateGraphCommand() {
 
-        }
-
+        GenerateGraphCommand::GenerateGraphCommand(const std::string &filename, const unsigned int vertexNumber,
+                                                   const unsigned int probability) : filename_(filename),
+                                                                                     vertexNumber_(vertexNumber),
+                                                                                     probability_(probability) {}
 
         void GenerateGraphCommand::execute() {
 
-        }
+        }//execute
+
     }//namespace command_handler
 
 } //namespace min_path
