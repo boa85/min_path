74d15aee7e1e01c93effb53cfaa9afc44600d1fd
boa <boa@promobit.ru>
2017-08-11 16:15:31 +0600

    added class GraphBuilder

---
+++ command_handler/include/graph_builder/graph_builder.hpp
@@ -0,0 +1,59 @@
+//
+
+#ifndef MIN_PATH_GRAPH_BUILDER
+#define MIN_PATH_GRAPH_BUILDER
+
+#include <list>
+#include <string>
+#include <memory>
+
+#include "../../../service/include/service.hpp"
+
+namespace min_path {
+
+    namespace graph_builder {
+        using namespace service;
+
+        struct Edge {
+            int inVertex;
+            int outVertex;
+            int cost;
+
+            bool isValid() {
+                return inVertex > 0 && outVertex > 0;
+            }
+
+        };
+
+        using Graph = std::list<Edge>;
+
+        class GraphBuilder {
+
+        public:
+            explicit GraphBuilder(const std::string &filename);
+
+            ~GraphBuilder() = default;
+
+            GraphBuilder(const GraphBuilder &) = default;
+
+            GraphBuilder(GraphBuilder &&) = default;
+
+            GraphBuilder &operator=(const GraphBuilder &) = default;
+
+            GraphBuilder &operator=(GraphBuilder &&) = default;
+
+
+        private:
+            std::string filename_;
+
+            std::shared_ptr<Graph> build();
+        };//class GraphBuilder
+
+
+    }//namespace graph_builder
+
+}//namespace min_path
+
+
+#endif //MIN_PATH_GRAPH_BUILDER
+
---
+++ command_handler/src/graph_builder/graph_builder.cpp
@@ -0,0 +1,38 @@
+#include <fstream>
+#include <boost/algorithm/string/split.hpp>
+#include<boost/algorithm/string.hpp>
+
+#include "../../include/graph_builder/graph_builder.hpp"
+
+namespace min_path {
+
+    namespace graph_builder {
+
+        GraphBuilder::GraphBuilder(const std::string &filename)
+                : filename_(filename) {}
+
+        std::shared_ptr<Graph> GraphBuilder::build() {
+            auto graph = std::make_shared<Graph>();
+            std::ifstream input(filename_);
+            std::string line;
+            while (getline(input, line)) {
+                std::vector<std::string> array;
+                boost::algorithm::split(array, line, boost::is_any_of(":"));
+                Edge edge{};
+                edge.inVertex = std::stoi(array[0]);
+                edge.cost = std::stoi(array[1]);
+                edge.outVertex = std::stoi(array[2]);
+                if (!edge.isValid()) {
+                    throw std::invalid_argument("invalid value");
+                }
+                graph->push_back(edge);
+            }
+            input.close();
+            return graph;
+
+        }//build
+
+    }//namespace graph_builder
+
+}//namespace min_path
+
