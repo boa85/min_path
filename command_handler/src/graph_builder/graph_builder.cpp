e0e1ea03f157294daa7efe1b22273e625c72f0cf
boa <boa@promobit.ru>
2017-08-12 14:06:01 +0600

    Added the calculation of the number of vertices in the graph

--- command_handler/src/graph_builder/graph_builder.cpp
+++ command_handler/src/graph_builder/graph_builder.cpp
@@ -15,6 +15,8 @@ namespace min_path {
             auto graph = std::make_shared<EdgesListGraphView>();
             std::ifstream input(filename_);
             std::string line;
+            int tmp1 = 0;
+            int tmp2 = 0;
             while (getline(input, line)) {//read next line
                 std::vector<std::string> array;
                 boost::algorithm::split(array, line, boost::is_any_of(":"));
@@ -28,9 +30,17 @@ namespace min_path {
                 if (!edge.isValid()) {
                     throw std::invalid_argument("invalid value edge");
                 }
+                tmp1 = std::max(edge.inVertex, tmp1);
+                tmp2 = std::max(edge.outVertex, tmp2);
                 graph->push_back(edge);
             }
+
+            edgeCount_ = graph->size();
+            vertexCount_ = std::max(tmp1, tmp2);
             input.close();
+            if (!isValidGraph(vertexCount_, edgeCount_)) {
+                throw std::logic_error("");
+            }
             return graph;
 
         }//buildEdgesListGraphView
@@ -39,7 +49,7 @@ namespace min_path {
             switch (graphFormat) {
                 case GRAPH_FORMAT::EDGE_LIST: {
                     auto graph = buildEdgesListGraphView();
-                    edgesListGraph(graph);
+                    edgesListGraph(graph, vertexCount_, edgeCount_);
                     break;
                 }
                 case GRAPH_FORMAT::ADJACENCY_MATRIX: {
@@ -48,17 +58,15 @@ namespace min_path {
                     break;
                 }
             }
-        }
+        }//getGraph
 
         std::shared_ptr<AdjacencyMatrixGraphView> GraphBuilder::buildAdjacencyMatrixGraphView() {
             return std::make_shared<AdjacencyMatrixGraphView>();
         }
 
-        bool GraphBuilder::isValidGragh(const unsigned int vertex, const unsigned int edge) {
+        bool GraphBuilder::isValidGraph(int vertex, unsigned long edge) {
             return (edge <= ((vertex * (vertex - 1)) / 2));//max edges count = n(n-1)/2, n - number of vertex
         }
-//getGraph
-
 
     }//namespace graph_builder
 
